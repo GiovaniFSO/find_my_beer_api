bbbd542b783dd3c0f8408144f8171b84da552a9e
Giovani Fernandes <giovani_oli@hotmail.com>
2021-10-02 05:02:24 -0400

    fix test create store and rating

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -1,21 +1,11 @@
 class Api::V1::ApiController < ActionController::API
   rescue_from ActiveRecord::RecordNotFound, with: :not_found
-  rescue_from ActiveRecord::RecordInvalid, with: :record_invalid
-  rescue_from ActionController::ParameterMissing, with: :parameter_missing
   rescue_from NoMethodError, with: :no_method_error
 
   private
 
   def not_found
     head :not_found
-  end
-
-  def record_invalid(exception)
-    render json: exception.record.errors, status: :unprocessable_entity
-  end
-
-  def parameter_missing(exception)
-    render json: exception.record.errors, status: :precondition_failed
   end
 
   def no_method_error(exception)
--- app/controllers/api/v1/ratings_controller.rb
+++ app/controllers/api/v1/ratings_controller.rb
@@ -1,29 +1,18 @@
 class Api::V1::RatingsController < Api::V1::ApiController
-
   def create
     ActiveRecord::Base.transaction do
-      create_store
-      create_rating
+      @store = Store.find_or_create_by!(store_params)
+      @store.ratings.create!(rating_params)
 
-      render json: @rating
+      render json: @store.ratings.last
     end
   end
 
   private
 
-  def create_rating
-    @rating = Rating.new(rating_params)
-    @rating.store_id = @store.id
-    @rating.save!
-  end
-
-  def create_store
-    @store = Store.find_or_create_by!(
-      lonlat: "POINT(#{params[:store][:longitude].to_f} #{params[:store][:latitude].to_f})",
-      name: params[:store][:name],
-      address: params[:store][:address],
-      google_place_id: params[:store][:google_place_id]
-    )
+  def store_params
+    lonlat = "POINT (#{params[:store][:longitude].to_f} #{params[:store][:latitude].to_f})"
+    params.require(:store).permit(:name, :address, :google_place_id).merge(lonlat: lonlat)
   end
 
   def rating_params
