5a47768b05c36a35c5fc932633a56169836343c1
Giovani Fernandes <giovani_oli@hotmail.com>
2021-10-03 18:27:48 -0400

    move url web app CORS to env

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000'
+    origins ENV['URL_WEB_APP']
 
     resource '*',
       headers: :any,
